2951247d41c0568521ec1e1eb3684beccea13b14
noxussj <632922356@qq.com>
2021-09-13 20:22:26 +0800

    完成目录生成

--- plopfile.js
+++ plopfile.js
@@ -3,6 +3,8 @@
  */
 module.exports = (plop) => {
     const del = require('del')
+    const resolve = require('path').resolve
+    const fs = require('fs')
 
     del('plop-dist')
 
@@ -15,13 +17,44 @@ module.exports = (plop) => {
                 default: 'my-vue-app'
             }
         ],
-        actions: [
-            {
-                type: 'add',
-                path: `plop-dist/index.vue`,
-                templateFile: 'plop-templates/index.vue',
-                data: {}
-            }
-        ]
+        actions: () => {
+            const actions = []
+            const filesPath = getFiles('plop-templates/demo/')
+
+            filesPath.map((path) => {
+                console.log(path)
+                actions.push({
+                    type: 'add',
+                    path: `dist/${path}`,
+                    templateFile: `plop-templates/demo/${path}`,
+                    data: {}
+                })
+            })
+
+            return actions
+        }
     })
+
+    /**
+     * 获取指定目录所有文件
+     */
+    function getFiles(basePath) {
+        const filesPath = []
+
+        function savePath(path) {
+            const files = fs.readdirSync(path)
+
+            files.map((name) => {
+                if (fs.statSync(resolve(path, name)).isDirectory()) {
+                    savePath(`${path + name}/`)
+                } else {
+                    filesPath.push(`${path + name}`.replace(basePath, ''))
+                }
+            })
+        }
+
+        savePath(basePath)
+
+        return filesPath
+    }
 }
