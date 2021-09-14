f95e53eda487dcb8189290100470ea78d3b363fa
noxussj <632922356@qq.com>
2021-09-14 22:11:38 +0800

    完成询问功能

--- bin/cli.js
+++ bin/cli.js
@@ -8,57 +8,72 @@ const join = require('path').join
 const { readdirSync, writeFileSync } = require('../libs/readdir-sync.js')
 const { processArray } = require('../libs/async-map.js')
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'create a app',
-            default: 'my-vue-app'
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Create a app',
+        default: 'my-vue-app'
+    },
+    {
+        type: 'confirm',
+        name: 'npm',
+        message: 'Is install node_modules?',
+        default: false
+    },
+    {
+        type: 'list',
+        name: 'theme',
+        message: 'Please select the way you download',
+        when(answers) {
+            return answers.npm
+        },
+        choices: ['yarn', 'cnpm', 'npm']
+    }
+]
+
+inquirer.prompt(questions).then((anwsers) => {
+    const spinner = ora({
+        text: 'download files',
+        spinner: {
+            interval: 80,
+            frames: ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏']
         }
-    ])
-    .then((anwsers) => {
-        const spinner = ora({
-            text: 'download files',
-            spinner: {
-                interval: 80,
-                frames: ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏']
-            }
-        }).start()
+    }).start()
 
-        const tmpDir = join(__dirname, '../', 'templates/demo')
-        const destDir = process.cwd()
-        const filesPath = readdirSync(tmpDir)
-        const hbsList = ['package.json', 'index.html']
+    const tmpDir = join(__dirname, '../', 'templates/demo')
+    const destDir = process.cwd()
+    const filesPath = readdirSync(tmpDir)
+    const hbsList = ['package.json', 'index.html']
 
-        const asyncArray = []
+    const asyncArray = []
 
-        filesPath.forEach((file) => {
-            asyncArray.push(() => {
-                return new Promise((resolve) => {
-                    setTimeout(() => {
-                        spinner.text = 'download ' + file
+    filesPath.forEach((file) => {
+        asyncArray.push(() => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    spinner.text = 'download ' + file
 
-                        let content = fs.readFileSync(join(tmpDir, file), 'utf-8')
+                    let content = fs.readFileSync(join(tmpDir, file), 'utf-8')
 
-                        hbsList.map((fileName) => {
-                            if (file.indexOf(fileName) > -1) {
-                                const template = handlebars.compile(content)
-                                content = template(anwsers)
-                            }
-                        })
+                    hbsList.map((fileName) => {
+                        if (file.indexOf(fileName) > -1) {
+                            const template = handlebars.compile(content)
+                            content = template(anwsers)
+                        }
+                    })
 
-                        writeFileSync(join(destDir, file), content)
+                    writeFileSync(join(destDir, anwsers.name, file), content)
 
-                        resolve()
-                    }, 200)
-                })
+                    resolve()
+                }, 100)
             })
         })
-
-        processArray(asyncArray).then((res) => {
-            setTimeout(() => {
-                spinner.stop()
-            }, 1000)
-        })
     })
+
+    processArray(asyncArray).then((res) => {
+        setTimeout(() => {
+            spinner.stop()
+        }, 1000)
+    })
+})
